5482bacbe55417dd22a8f3b50f6edba2f65c7b2b
feripratama <eroorsys@gmail.com>
2018-04-18 21:35:30 +0700

    Update readme, ServiceProvider, Controller
     On branch master
     Changes to be committed:
    	modified:   README.md
    	modified:   src/Http/Controllers/SiswaPendaftaranController.php
    	modified:   src/SiswaPendaftaranServiceProvider.php

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # siswa-pendaftaran
 siswa-pendaftaran
+
+
+### install via composer
+```bash
+composer require bantenprov/siswa-pendaftaran:dev-master
+```
+
+### edit `config/app.php`
+
+```php
+'providers' => [
+    Bantenprov\SiswaPendaftaran\SiswaPendaftaranServiceProvider::class,
+```
+
+### artisan command
+
+```bash
+php artisan vendor:publish --tag=siswa-pendaftaran-assets
+```
--- src/Http/Controllers/SiswaPendaftaranController.php
+++ src/Http/Controllers/SiswaPendaftaranController.php
@@ -18,35 +18,4 @@ class SiswaPendaftaranController extends Controller
     {
 
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $response = [];
-        $kegiatan = $this->kegiatanModel->all();
-        $users_special = $this->user->all();
-        $users_standar = $this->user->find(\Auth::User()->id);
-        $current_user = \Auth::User();
-        $role_check = \Auth::User()->hasRole(['superadministrator','administrator']);
-        if($role_check){
-            $response['user_special'] = true;
-            foreach($users_special as $user){
-                array_set($user, 'label', $user->name);
-            }
-            $response['user'] = $users_special;
-        }else{
-            $response['user_special'] = false;
-            array_set($users_standar, 'label', $users_standar->name);
-            $response['user'] = $users_standar;
-        }
-        array_set($current_user, 'label', $current_user->name);
-        $response['current_user'] = $current_user;
-        $response['kegiatan'] = $kegiatan;
-        $response['status'] = true;
-        return response()->json($response);
-    }
 }
